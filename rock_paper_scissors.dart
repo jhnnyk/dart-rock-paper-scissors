b2541a1ae55e95dcc56c553c57e151e0fa97bb34
John Knotts <jhnnyk@gmail.com>
2024-08-28 13:27:43 -0600

    implement the game loop

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,7 +1,16 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Rock, Paper or Scissors? (r/p/s) ');
-  final input = stdin.readLineSync();
-  print(input);
+  while (true) {
+    stdout.write('Rock, Paper or Scissors? (r/p/s) ');
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      print('Selected: $input');
+    } else if (input == 'q') {
+      print('Goodbye');
+      break;
+    } else {
+      print('Invalid input. Type r, p, or s or q to quit.');
+    }
+  }
 }
